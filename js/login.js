41141fda02391922ab072954d1687b2fdf418d4e
kayageum <kayageum@gmail.com>
2022-04-24 16:58:35 +0900

    add login

--- js/login.js
+++ js/login.js
@@ -1,9 +1,28 @@
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeging = document.querySelector("#greeting");
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 function loginHandler(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  showGreeging(username);
 }
 
-loginForm.addEventListener("submit", loginHandler);
+function showGreeging(username) {
+  greeging.classList.remove(HIDDEN_CLASSNAME);
+  greeging.innerText = "Hello " + username;
+}
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", loginHandler);
+} else {
+  showGreeging(savedUsername);
+}
